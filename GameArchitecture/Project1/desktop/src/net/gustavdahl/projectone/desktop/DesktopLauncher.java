3757dce0fb1902c45b03eb39b31ec7f7b9591e3c
Wikzo <Linkthewindwaker@gmail.com>
2015-10-04 18:42:55 +0200

    First commit

---
+++ GameArchitecture/Project1/desktop/src/net/gustavdahl/projectone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.gustavdahl.projectone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.gustavdahl.projectone.Project1;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Project1(), config);
+	}
+}
