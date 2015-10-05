4c8a6db2836aab4c0d6aa205b8edc2060aa52835
Wikzo <Linkthewindwaker@gmail.com>
2015-10-05 12:14:40 +0200

    Visualization added

--- GameArchitecture/BucketDropGame/core/src/net/gustavdahl/bucketdrop/BucketDrop.java
+++ GameArchitecture/BucketDropGame/core/src/net/gustavdahl/bucketdrop/BucketDrop.java
@@ -47,7 +47,7 @@ public class BucketDrop implements ApplicationListener
 	@Override
 	public void create()
 	{
-		dropImage = new Texture("droplet.png");
+		dropImage = new Texture("cat-tongue.png");
 		bucketImage = new Texture("bucket.png");
 
 		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
