dc47788a2be1f1f31b7b2811e2bebde466ce77e2
Wikzo <Linkthewindwaker@gmail.com>
2015-10-12 08:28:44 +0200

    Node JS, R, Shaders

---
+++ Advanced Computer Graphics/ACGR_Lab1/Binaries/launch-tutorial09_vbo_indexing-RelWithDebInfo.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/Wikzo/Documents/Hagenberg_2015/Advanced Computer Graphics/ACGR_Lab1/tutorial09_vbo_indexing/
+set PATH=;%PATH%
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\Wikzo\Documents\Hagenberg_2015\Advanced Computer Graphics\ACGR_Lab1\Binaries\RelWithDebInfo\tutorial09_vbo_indexing.exe"  
+pause
