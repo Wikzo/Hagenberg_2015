2d84dd0c9f27ea627b8959802d000f8edb4f600e
Wikzo <Linkthewindwaker@gmail.com>
2016-01-15 22:41:58 +0100

    Twitter Mood Map

--- SemanticTextAnalysis/Twitter_MyMoodMap/hbc-example/src/main/java/com/twitter/hbc/example/TwitterMoodMap.java
+++ SemanticTextAnalysis/Twitter_MyMoodMap/hbc-example/src/main/java/com/twitter/hbc/example/TwitterMoodMap.java
@@ -1,25 +1,14 @@
-/**
- * Copyright 2013 Twitter, Inc.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
-
 package com.twitter.hbc.example;
 
 import java.io.Console;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -48,6 +37,11 @@ public class TwitterMoodMap
 
 	static TextAPIClient textApiClient = new TextAPIClient("87aa215a", "7a4e24952e4f20db11fc1508abd12eac");
 
+	static String searchTerm = "";
+	static int numberOfTweets = 0;
+	static boolean infiniteNumberOfTweets = false;
+	static String found = "";
+
 	public static void run(String consumerKey, String consumerSecret, String token, String secret)
 			throws InterruptedException, JSONException, IOException, TextAPIException
 	{
@@ -57,7 +51,7 @@ public class TwitterMoodMap
 		BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
 		StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
 		// add some track terms
-		endpoint.trackTerms(Lists.newArrayList("Rickman"));
+		endpoint.trackTerms(Lists.newArrayList(searchTerm));
 		// endpoint.getURI();
 
 		Authentication auth = new OAuth1(consumerKey, consumerSecret, token, secret);
@@ -75,11 +69,12 @@ public class TwitterMoodMap
 
 		JSONObject outputJson = new JSONObject();
 		JSONArray jsonArray = new JSONArray();
-
 		twitter4j.JSONObject twitterJSON;
+		int validatedTweets = 0;
 
-		// Do whatever needs to be done with messages
-		for (int validatedTweets = 0; validatedTweets < 100;)
+		// gather the tweets
+		while (validatedTweets < numberOfTweets || infiniteNumberOfTweets)
+		// for (int validatedTweets = 0; validatedTweets < numberOfTweets;)
 		{
 			// get next json message
 			String msg = queue.take();
@@ -101,11 +96,11 @@ public class TwitterMoodMap
 			// validate if it has both a text and a location
 			if (text != null && location != null)
 			{
+				// System.out.println(text);
+				String originalLocation = location;
 				location = ValidateData(text, location);
 				if (location != "")
 				{
-
-					// validatedTweets++;
 
 					text = ClearTheString(text);
 
@@ -113,30 +108,30 @@ public class TwitterMoodMap
 					myJson.put("Location", location);//
 					// myJson.put("Text", text);
 
+					Thread.sleep(100);
 					String mood = SentimentAnalysis(text, textApiClient);
 
 					if (mood != "")
 					{
+
 						validatedTweets++;
 
 						myJson.put("Mood", mood);
 
 						jsonArray.add(myJson);
 
-						System.out.println(myJson.toString());
-						
-						
-						System.out.println("Gathered tweets: " + validatedTweets);
+						found = "Found: " + myJson.toString() + " - Original location: " + originalLocation + " - Text: " + text;
+						System.out.println(found);
+
+						// validatedTweets);
 						WriteToJSONFile(jsonArray);
 					}
 
-					
-
 				}
-
 				
-				//System.out.println("Gathered tweets: " + validatedTweets);
-				// Thread.sleep(1000);
+				
+
+				System.out.println("Gathered tweets: " + validatedTweets);
 
 			}
 		}
@@ -151,14 +146,11 @@ public class TwitterMoodMap
 	{
 
 		// http://crunchify.com/how-to-write-json-object-to-file-in-java/
-		// System.out.println(jsonArray);
-
-		// try-with-resources statement based on post comment below :)
 		try (FileWriter file = new FileWriter("tweets.json"))
 		{
 			file.write(jsonArray.toJSONString());
-			//System.out.println("Successfully Copied JSON Object to File...");
-			//System.out.println("\nJSON Object: " + jsonArray);
+			// System.out.println("Successfully Copied JSON Object to File...");
+			// System.out.println("\nJSON Object: " + jsonArray);
 
 		}
 	}
@@ -181,29 +173,82 @@ public class TwitterMoodMap
 	{
 		String l = location.trim().toLowerCase();
 
-		String validatedString = l;
-		String[] validLocations =
-		{ "ireland", "newzeland", "australia", "norway", "denmark", "sweden", "canada", "singapore" };
+		//System.out.println(l);
 
+		String validatedString = l;
 		boolean valid = false;
-		for (int i = 0; i < validLocations.length; i++)
+
+		// Netherlands
+		if (l.contains("netherlands") || l.contains("holland") || l.contains("amsterdam") || l.contains("rotterdam")
+				|| l.contains("utrecht"))
 		{
-			if (l.equalsIgnoreCase(validLocations[i]))
-				valid = true;
+			valid = true;
+			validatedString = "amsterdam";
 		}
 
-		// USA
-		if (l.equalsIgnoreCase("us") || l.equalsIgnoreCase("usa") || l.equalsIgnoreCase("america")
-				|| l.equalsIgnoreCase("unitedstates") || l.equalsIgnoreCase("northamerica"))
+		// Canada
+		if (l.contains("canada") || l.contains("danmark") || l.equalsIgnoreCase("british columbia")
+				|| l.contains("quebec") || l.contains("toronto") || l.contains("ontario") || l.contains("Winnipeg"))
+		{
+			valid = true;
+			validatedString = "canada";
+		}
+
+		// Denmark
+		if (l.contains("denmark") || l.contains("danmark") || l.contains("copenhagen") || l.contains("aalborg"))
+		{
+			valid = true;
+			validatedString = "denmark";
+		}
+
+		// Sweden
+		if (l.contains("sweden") || l.contains("stockholm") || l.contains("gothenburg") || l.contains("malmo"))
+		{
+			valid = true;
+			validatedString = "sweden";
+		}
+
+		// Norway
+		if (l.contains("norway") || l.contains("oslo") || l.contains("bergen") || l.contains("trondheim"))
+		{
+			valid = true;
+			validatedString = "norway";
+		}
+
+		// Australia
+		if (l.contains("australia") || l.contains("sidney") || l.contains("melbourne") || l.contains("brisbane")
+				|| l.contains("perth"))
+		{
+			valid = true;
+			validatedString = "australia";
+		}
+
+		// Ireland
+		if (l.contains("ireland") || l.contains("dublin") || l.contains("cork") || l.contains("belfast"))
+		{
+			valid = true;
+			validatedString = "ireland";
+		}
+
+		// New Zealand
+		if (l.contains("newzeland") || l.contains("auckland") || l.contains("wellington"))
+		{
+			valid = true;
+			validatedString = "newzeland";
+		}
+
+		// USA (special case)
+		if (l.contains("us") || l.contains("usa") || l.contains("america") || l.contains("los angeles")
+				|| l.contains("california") || l.contains("york") || l.equalsIgnoreCase("unitedstates")
+				|| l.equalsIgnoreCase("newyork") || l.contains("washington"))
 		{
 			valid = true;
 			validatedString = "usa";
 		}
 
-		// UK
-		if (l.equalsIgnoreCase("uk") || l.equalsIgnoreCase("england") || l.equalsIgnoreCase("uk")
-				|| l.equalsIgnoreCase("britain") || l.equalsIgnoreCase("greatbritain")
-				|| l.equalsIgnoreCase("unitedkingdom") || l.equalsIgnoreCase("london"))
+		// UK (special case)
+		if (l.contains("uk") || l.contains("england") || l.contains("uk") || l.contains("u.k.") || l.contains("britain")
+				|| l.contains("great britain") || l.equalsIgnoreCase("unitedkingdom") || l.contains("london"))
 		{
 			valid = true;
 			validatedString = "uk";
@@ -239,6 +284,19 @@ public class TwitterMoodMap
 
 	public static void main(String[] args) throws InterruptedException, JSONException, IOException, TextAPIException
 	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Write your search term:");
+		searchTerm = keyboard.nextLine();
+
+		System.out.println("Write how many tweets you want to gather ('-1' is infinite):");
+		numberOfTweets = keyboard.nextInt();
+
+		if (numberOfTweets <= -1)
+		{
+			numberOfTweets = 1;
+			infiniteNumberOfTweets = true;
+		}
+
 		run("g8V1ywv6SEJphCGseyWagmeoy", "7zX8CHnstU9gUBFC3qRVClsKG9x8MqdQbuReMb3q1AR4VeAkS1",
 				"14982223-t7GxCHppDi3FQzuCedS3H95r39WgtUNOhvKp1DYcR", "Nj8Vu4R8Vn11NXMa5jH0iTiWfroLleQwSj71Tb4BPMTsi");
 
@@ -261,18 +319,17 @@ public class TwitterMoodMap
 
 			Sentiment sentiment = textAPIClient.sentiment(builder.build());
 
-			
 			String polarity = sentiment.getPolarity();
-			//System.out.println(polarity);
-			
+			// System.out.println(polarity);
+
 			if (polarity.equals("positive"))
 				return "1";
 			else if (polarity.equals("negative"))
 				return "-1";
 			else
 				return "0";
-			
-			//return sentiment.getPolarity();
+
+			// return sentiment.getPolarity();
 			// System.out.println(score);
 
 			// good 0.8693919697854208
